483e26b85149d5fe980e46d423ed639053fe36f4
Leandro Mendes <lelecoqzy@gmail.com>
2025-06-05 20:55:05 -0300

    Implement user input for name in Main class

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Greeter greeter = new Greeter();
-        System.out.println(greeter.greet("Leandro"));
-        System.out.println(greeter.greet("")); // Testing with an empty string
+
+        System.out.println("--- Personal Greeter ---");
+        System.out.print("Please type your name: ");
+        String userName = scanner.nextLine();
+
+        System.out.println("Nome lido: " + userName);
+        System.out.println(greeter.greet("World")); // Generic greeting
+
+        scanner.close();
     }
 }
