a0b9d64d2b3b9a698e4ac115b8f03326e82a38c9
Leandro Mendes <lelecoqzy@gmail.com>
2025-06-05 20:51:27 -0300

    Create Greeter class with basic greeting

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.greet());
     }
 }
